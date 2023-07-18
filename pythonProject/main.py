37aa5dda163c4ce7a4a11ca75924402fe623c5aa
Eren Arabacı <erenarabaci@windowslive.com>
2023-07-18 18:59:54 +0300

    For Dongüleri aynı anda başlamalı

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,9 +1,40 @@
 import turtle
 import time
+import random
+import threading
 
 board = turtle.Screen()
 board.bgcolor("black")
 board.title("CatchTheTurtle")
+
+
+
+
+xcolors=["red","blue","yellow","purple","pink","SpringGreen","WhiteSmoke","turquoise","orange","maroon"]
+
+
+
+
+x = turtle.Turtle()
+x.shape("turtle")
+x.color("yellow")
+x.hideturtle()
+x.penup()
+def randommoves():
+    for run in range(15):
+        a = random.randint(-200,200)
+        b = random.randint(-200,200)
+
+
+        x.goto(a,b)
+        x.showturtle()
+        time.sleep(1)
+        x.hideturtle()
+
+
+
+
+
 ########score string#######
 score=turtle.Turtle()
 score.color("Green")
@@ -34,11 +65,32 @@ sayacstring.write("Sayaç :",font=("Courier",24,"normal"))
 sayacstring.color("black")
 
 
+def countdown():
+    for i in range(30, -1, -1):
+        sayac.clear()  # Önceki sayıyı temizle
+        sayac.write(i, align="center", font=("Courier", 24, "normal"))
+        time.sleep(1)  # 1 saniye bekle
 
-for i in range(30, -1, -1):
-    sayac.clear()  # Önceki sayıyı temizle
-    sayac.write(i, align="center", font=("Courier", 24, "normal"))
-    time.sleep(1)  # 1 saniye bekle
+
+thread1 = threading.Thread(target=randommoves())
+thread2 = threading.Thread(target=countdown())
+thread1.start()
+thread2.start()
+
+thread1.join()
+thread2.join()
+
+
+#########Kaplumbağa yakalama###############
+
+
+
+
+
+
+
+
+
 
 
 
@@ -49,9 +101,9 @@ turtle.mainloop()
 
 
 
-for i in range(30):
-    time.sleep(1)
-    print(i)
+
+
+
 
 
 
